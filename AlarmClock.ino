62002b32b525214633897156a0dd444380338907
Antoine Chabert <chabert.antoine@gmail.com>
2017-04-11 21:15:17 +0200

    Add lib to manage digits

--- AlarmClock.ino
+++ AlarmClock.ino
@@ -1,4 +1,6 @@
 #include "TimerOne.h"
+#include "SevSeg.h"
+SevSeg sevseg;
 
 //display pins
 int segA = 2;
@@ -9,7 +11,6 @@ int segE = 6;
 int segF = 7;
 int segG = 8;
 int segP = 9;
-
 int d1 = 10;
 int d2 = 11;
 int d3 = 12;
@@ -18,19 +19,16 @@ int d4 = 13;
 int time = 0;
 
 void setup() {
-  //set up outputs
-  pinMode(segA, OUTPUT);
-  pinMode(segB, OUTPUT);
-  pinMode(segC, OUTPUT);
-  pinMode(segD, OUTPUT);
-  pinMode(segE, OUTPUT);
-  pinMode(segF, OUTPUT);
-  pinMode(segG, OUTPUT);
-  pinMode(segP, OUTPUT);
-  pinMode(d1, OUTPUT);
-  pinMode(d2, OUTPUT);
-  pinMode(d3, OUTPUT);
-  pinMode(d4, OUTPUT);
+  // Set up segments
+  byte numDigits = 4;
+  byte digitPins[] = {d1, d2, d3, d4};
+  byte segmentPins[] = {segA, segB, segC, segD, segE, segF, segG, segP};
+  bool resistorsOnSegments = false;
+  byte hardwareConfig = COMMON_CATHODE;
+  bool updateWithDelays = false;
+  bool leadingZeros = false;
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments, updateWithDelays, leadingZeros);
+  sevseg.setBrightness(90);
   
   // Init timer
   Timer1.initialize(1000000);
@@ -38,241 +36,13 @@ void setup() {
 }
 
 void loop() {
-  int minutes = time / 60;
-  int secondes = time % 60;
-
-  selectDigit(1);
-  sendDigit(int(minutes / 10));
-
-
-  selectDigit(2);
-  sendDigit(minutes % 10);
-  point();
-
-  selectDigit(3);
-  sendDigit(int(secondes / 10));
-
-  selectDigit(4);
-  sendDigit(secondes % 10);
+  int minutes = (time / 60) * 100;
+  int seconds = time % 60;
+  int displayTime = minutes + seconds;
+  sevseg.setNumber(displayTime, 2);
+  sevseg.refreshDisplay();
 }
 
 void incrementTimer() {
   time++;
-}
-
-void allLow() {
-  digitalWrite(segA, LOW);
-  digitalWrite(segB, LOW);
-  digitalWrite(segC, LOW);
-  digitalWrite(segD, LOW);
-  digitalWrite(segE, LOW);
-  digitalWrite(segF, LOW);
-  digitalWrite(segG, LOW);
-  digitalWrite(segP, LOW);
-  digitalWrite(d1, LOW);
-  digitalWrite(d2, LOW);
-  digitalWrite(d3, LOW);
-  digitalWrite(d4, LOW);
-}
-
-void allHigh() {
-  digitalWrite(segA, HIGH);
-  digitalWrite(segB, HIGH);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, HIGH);
-  digitalWrite(segE, HIGH);
-  digitalWrite(segF, HIGH);
-  digitalWrite(segG, HIGH);
-  digitalWrite(segP, HIGH);
-  digitalWrite(d1, HIGH);
-  digitalWrite(d2, HIGH);
-  digitalWrite(d3, HIGH);
-  digitalWrite(d4, HIGH);
-}
-
-void one() {
-  digitalWrite(segA, LOW);
-  digitalWrite(segB, HIGH);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, LOW);
-  digitalWrite(segE, LOW);
-  digitalWrite(segF, LOW);
-  digitalWrite(segG, LOW);
-  digitalWrite(segP, LOW);
-}
-
-void two() {
-  digitalWrite(segA, HIGH);
-  digitalWrite(segB, HIGH);
-  digitalWrite(segC, LOW);
-  digitalWrite(segD, HIGH);
-  digitalWrite(segE, HIGH);
-  digitalWrite(segF, LOW);
-  digitalWrite(segG, HIGH);
-  digitalWrite(segP, LOW);
-}
-
-void three() {
-  digitalWrite(segA, HIGH);
-  digitalWrite(segB, HIGH);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, HIGH);
-  digitalWrite(segE, LOW);
-  digitalWrite(segF, LOW);
-  digitalWrite(segG, HIGH);
-  digitalWrite(segP, LOW);
-}
-
-void four() {
-  digitalWrite(segA, LOW);
-  digitalWrite(segB, HIGH);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, LOW);
-  digitalWrite(segE, LOW);
-  digitalWrite(segF, HIGH);
-  digitalWrite(segG, HIGH);
-  digitalWrite(segP, LOW);
-}
-
-void five() {
-  digitalWrite(segA, HIGH);
-  digitalWrite(segB, LOW);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, HIGH);
-  digitalWrite(segE, LOW);
-  digitalWrite(segF, HIGH);
-  digitalWrite(segG, HIGH);
-  digitalWrite(segP, LOW);
-}
-
-void six() {
-  digitalWrite(segA, HIGH);
-  digitalWrite(segB, LOW);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, HIGH);
-  digitalWrite(segE, HIGH);
-  digitalWrite(segF, HIGH);
-  digitalWrite(segG, HIGH);
-  digitalWrite(segP, LOW);
-}
-
-void seven() {
-  digitalWrite(segA, HIGH);
-  digitalWrite(segB, HIGH);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, LOW);
-  digitalWrite(segE, LOW);
-  digitalWrite(segF, LOW);
-  digitalWrite(segG, LOW);
-  digitalWrite(segP, LOW);
-}
-
-void eight() {
-  digitalWrite(segA, HIGH);
-  digitalWrite(segB, HIGH);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, HIGH);
-  digitalWrite(segE, HIGH);
-  digitalWrite(segF, HIGH);
-  digitalWrite(segG, HIGH);
-  digitalWrite(segP, LOW);
-}
-
-void nine() {
-  digitalWrite(segA, HIGH);
-  digitalWrite(segB, HIGH);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, HIGH);
-  digitalWrite(segE, LOW);
-  digitalWrite(segF, HIGH);
-  digitalWrite(segG, HIGH);
-  digitalWrite(segP, LOW);
-}
-
-void zero() {
-  digitalWrite(segA, HIGH);
-  digitalWrite(segB, HIGH);
-  digitalWrite(segC, HIGH);
-  digitalWrite(segD, HIGH);
-  digitalWrite(segE, HIGH);
-  digitalWrite(segF, HIGH);
-  digitalWrite(segG, LOW);
-  digitalWrite(segP, LOW);
-
-}
-
-void point() {
-  digitalWrite(segP, HIGH);
-  clearSegments();
-}
-
-void selectDigit(int d) {
-  digitalWrite(d1, HIGH);
-  digitalWrite(d2, HIGH);
-  digitalWrite(d3, HIGH);
-  digitalWrite(d4, HIGH);
-
-  switch (d) {
-  case 1:
-    digitalWrite(d1, LOW);
-    break;
-  case 2:
-    digitalWrite(d2, LOW);
-    break;
-  case 3:
-    digitalWrite(d3, LOW);
-    break;
-  case 4:
-    digitalWrite(d4, LOW);
-    break;
-  default:
-    break;
-  }
-}
-
-void sendDigit(int x) {
-  switch(x) {
-  case 1:
-    one();
-    break;
-  case 2:
-    two();
-    break;
-  case 3:
-    three();
-    break;
-  case 4:
-    four();
-    break;
-  case 5:
-    five();
-    break;
-  case 6:
-    six();
-    break;
-  case 7:
-    seven();
-    break;
-  case 8:
-    eight();
-    break;
-  case 9:
-    nine();
-    break;
-  default:
-    zero();
-    break;
-  }
-  clearSegments();
-}
-
-void clearSegments() {
-  digitalWrite(segA, LOW);
-  digitalWrite(segB, LOW);
-  digitalWrite(segC, LOW);
-  digitalWrite(segD, LOW);
-  digitalWrite(segE, LOW);
-  digitalWrite(segF, LOW);
-  digitalWrite(segG, LOW);
-  digitalWrite(segP, LOW);
 }
